3cbdf73b823ae7641fd04eeed4703867d3ffe490
phil-huynh <philip.huynh@galvanize.com>
2024-03-20 07:27:28 -0400

    debugged query

--- src/components/customHooks/useLaunchesQuery.ts
+++ src/components/customHooks/useLaunchesQuery.ts
@@ -27,6 +27,27 @@ export default function useLaunchesQuery() {
       $video_link: Boolean!,
       $wikipedia: Boolean!,
       $rocket: Boolean!,
+      $rocket_details: Boolean!,
+      $active: Boolean!,
+      $boosters: Boolean!,
+      $company: Boolean!,
+      $cost_per_launch: Boolean!,
+      $country: Boolean!,
+      $description: Boolean!,
+      $diameter: Boolean!,
+      $engines: Boolean!,
+      $first_flight: Boolean!,
+      $first_stage: Boolean!,
+      $height: Boolean!,
+      $landing_legs: Boolean!,
+      $mass: Boolean!,
+      $name: Boolean!,
+      $payload_weights: Boolean!,
+      $second_stage: Boolean!,
+      $stages: Boolean!,
+      $type: Boolean!,
+      $rocket_wikipedia: Boolean!,
+      $success_rate_pct: Boolean!,
 
       $details: Boolean!
     ){
@@ -49,6 +70,97 @@ export default function useLaunchesQuery() {
         rocket @include(if: $rocket){
           rocket_name
           rocket_type
+          rocket @include(if: $rocket_details){
+            active @include(if: $active)
+            boosters @include(if: $boosters)
+            company @include(if: $company)
+            cost_per_launch @include(if: $cost_per_launch)
+            country @include(if: $country)
+            description @include(if: $description)
+            diameter @include(if: $diameter){
+              feet
+              meters
+            }
+            engines @include(if: $engines){
+              engine_loss_max
+              layout
+              number
+              propellant_1
+              propellant_2
+              thrust_sea_level {
+                kN
+                lbf
+              }
+              thrust_to_weight
+              thrust_vacuum {
+                kN
+                lbf
+              }
+              type
+              version
+            }
+            first_flight @include(if: $first_flight)
+            first_stage @include(if: $first_stage) {
+              burn_time_sec
+              engines
+              fuel_amount_tons
+              reusable
+              thrust_sea_level {
+                kN
+                lbf
+              }
+              thrust_vacuum {
+                kN
+                lbf
+              }
+            }
+            height @include(if: $height) {
+              feet
+              meters
+            }
+            id
+            landing_legs @include(if: $landing_legs) {
+              material
+              number
+            }
+            mass @include(if: $mass) {
+              kg
+              lb
+            }
+            name @include(if: $name)
+            payload_weights @include(if: $payload_weights) {
+              id
+              kg
+              lb
+              name
+            }
+            second_stage @include(if: $second_stage) {
+              burn_time_sec
+              engines
+              fuel_amount_tons
+              payloads {
+                composite_fairing {
+                  diameter {
+                    meters
+                    feet
+                  }
+                  height {
+                    feet
+                    meters
+                  }
+                }
+                option_1
+              }
+              thrust {
+                kN
+                lbf
+              }
+            }
+            stages @include(if: $stages)
+            type @include(if: $type)
+            wikipedia @include(if: $rocket_wikipedia)
+            success_rate_pct @include(if: $success_rate_pct)
+          }
         }
         details @include(if: $details)
       }
@@ -67,12 +179,21 @@ export default function useLaunchesQuery() {
     launchToggles.rocket &&
       Object.keys(rocketToggles)
         .slice(1)
-        .filter((key: string) => linkToggles[key])
+        .filter((key: string) => rocketToggles[key])
         .length > 0
   )
 
+  console.log("Can get rocket details", canGetRocketDetails)
+  console.log(launchToggles.rocket)
+  console.log(Object.keys(rocketToggles)
+  .slice(1)
+  .filter((key: string) => linkToggles[key])
+  .length )
 
 console.log("vars")
+
+console.log(rocketToggles.name)
+
   const { loading, error, data } = useQuery(GET_LAUNCHES, {
     variables: {
       mission_name: launchToggles.mission_name,
@@ -89,36 +210,34 @@ console.log("vars")
       video_link: linkToggles.video_link,
       wikipedia: linkToggles.wikipedia,
       rocket: launchToggles.rocket,
-
-      // rocket_details: canGetRocketDetails,
-      // active: rocketToggles.active,
-      // boosters: rocketToggles.boosters,
-      // company: rocketToggles.company,
-      // cost_per_launch: rocketToggles.cost_per_launch,
-      // country: rocketToggles.country,
-      // description: rocketToggles.description,
-      // diameter: rocketToggles.diameter,
-      // engines: rocketToggles.engines,
-      // first_flight: rocketToggles.first_flight,
-      // first_stage: rocketToggles.first_stage,
-      // height: rocketToggles.height,
-      // landing_legs: rocketToggles.landing_legs,
-      // mass: rocketToggles.mass,
-      // name: rocketToggles.name,
-      // payload_weights: rocketToggles.payload_weights,
-      // second_stage: rocketToggles.second_stage,
-      // stages: rocketToggles.stages,
-      // type: rocketToggles.type,
-      // rocket_wikipedia: rocketToggles.wikipedia,
-      // success_rate_pct: rocketToggles.success_rate_pct,
-
-
+      rocket_details: canGetRocketDetails,
+      active: rocketToggles.active,
+      boosters: rocketToggles.boosters,
+      company: rocketToggles.company,
+      cost_per_launch: rocketToggles.cost_per_launch,
+      country: rocketToggles.country,
+      description: rocketToggles.description,
+      diameter: rocketToggles.diameter,
+      engines: rocketToggles.engines,
+      first_flight: rocketToggles.first_flight,
+      first_stage: rocketToggles.first_stage,
+      height: rocketToggles.height,
+      landing_legs: rocketToggles.landing_legs,
+      mass: rocketToggles.mass,
+      name: rocketToggles.name,
+      payload_weights: rocketToggles.payload_weights,
+      second_stage: rocketToggles.second_stage,
+      stages: rocketToggles.stages,
+      type: rocketToggles.type,
+      rocket_wikipedia: rocketToggles.wikipedia,
+      success_rate_pct: rocketToggles.success_rate_pct,
       details: launchToggles.details,
     }
   })
 
   let headers: string[] | null = null
   if (data) {
+    console.log(data.launchesPast)
     launchList.current = data.launchesPast.slice().reverse()
     headers = Object.keys(data?.launchesPast[0])
       .slice(1)
@@ -141,120 +260,28 @@ console.log("vars")
 
 
 
-// $rocket_details: Boolean!,
-// $active: Boolean!,
-// $boosters: Boolean!,
-// $company: Boolean!,
-// $cost_per_launch: Boolean!,
-// $country: Boolean!,
-// $description: Boolean!,
-// $diameter: Boolean!,
-// $engines: Boolean!,
-// $first_flight: Boolean!,
-// $first_stage: Boolean!,
-// $height: Boolean!,
-// $landing_legs: Boolean!,
-// $mass: Boolean!,
-// $name: Boolean!,
-// $payload_weights: Boolean!,
-// $second_stage: Boolean!,
-// $stages: Boolean!,
-// $type: Boolean!,
-// $rocket_wikipedia: Boolean!,
-// $success_rate_pct: Boolean!,
 
 
 
 
 
-// rocket @include(if: $rocket_details){
-//   active @include(if: $active)
-//   boosters @include(if: $boosters)
-//   company @include(if: $company)
-//   cost_per_launch @include(if: $cost_per_launch)
-//   country @include(if: $country)
-//   description @include(if: $description)
-//   diameter @include(if: $diameter){
-//     feet
-//     meters
-//   }
-//   engines @include(if: $engines){
-//     engine_loss_max
-//     layout
-//     number
-//     propellant_1
-//     propellant_2
-//     thrust_sea_level {
-//       kN
-//       lbf
-//     }
-//     thrust_to_weight
-//     thrust_vacuum {
-//       kN
-//       lbf
-//     }
-//     type
-//     version
-//   }
-//   first_flight @include(if: $first_flight)
-//   first_stage @include(if: $first_stage) {
-//     burn_time_sec
-//     engines
-//     fuel_amount_tons
-//     reusable
-//     thrust_sea_level {
-//       kN
-//       lbf
-//     }
-//     thrust_vacuum {
-//       kN
-//       lbf
-//     }
-//   }
-//   height @include(if: $height) {
-//     feet
-//     meters
-//   }
-//   id
-//   landing_legs @include(if: $landing_legs) {
-//     material
-//     number
-//   }
-//   mass @include(if: $mass) {
-//     kg
-//     lb
-//   }
-//   name
-//   payload_weights @include(if: payload_weights) {
-//     id
-//     kg
-//     lb
-//     name
-//   }
-//   second_stage @include(if: second_stage) {
-//     burn_time_sec
-//     engines
-//     fuel_amount_tons
-//     payloads {
-//       composite_fairing {
-//         diameter {
-//           meters
-//           feet
-//         }
-//         height {
-//           feet
-//           meters
-//         }
-//       }
-//       option_1
-//     }
-//     thrust {
-//       kN
-//       lbf
-//     }
-//   }
-//   stages @include(if: stages)
-//   type @include(if: type)
-//   wikipedia @include(if: rocket_wikipedia)
-//   success_rate_pct @include(if: success_rate_pct)
-// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// name
+
+
+
